7dfcdfe156256dcf3beee5342e69259f004859a5
jahn18 <john.ahn.es@gmail.com>
2021-01-06 18:00:14 -0800

    Added headers for my social media links

--- app.js
+++ app.js
@@ -3,18 +3,8 @@ let modalButton = document.getElementById('motion-design-card');
 let closeButton = document.getElementById('closeBtn');
 let overlay = document.getElementById('overlay');
 
-let contactModal = document.getElementById('contact-me-modal');
-let contactMeButton = document.getElementById('contact-me');
-
-contactMeButton.addEventListener('click', openContactInfo)
 modalButton.addEventListener('click', openModal);
 closeButton.addEventListener('click', closeModal);
-
-function openContactInfo() {
-    contactModal.classList.add('active');
-    overlay.classList.add('active');
-    document.body.style.overflow = "hidden";
-}
 
 function openModal() {
     modal.classList.add('active');
@@ -24,7 +14,7 @@ function openModal() {
 
 function closeModal() {
     modal.classList.remove('active');
-    contactModal.classList.remove('active');
+    // contactModal.classList.remove('active');
     overlay.classList.remove('active');
     document.body.style.overflow = "auto";
 }
