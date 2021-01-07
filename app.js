078f231dd294efc9a97ae6e85ab77d164475b567
jahn18 <john.ahn.es@gmail.com>
2021-01-06 17:21:50 -0800

    Implemented the contact me button

--- app.js
+++ app.js
@@ -3,8 +3,18 @@ let modalButton = document.getElementById('motion-design-card');
 let closeButton = document.getElementById('closeBtn');
 let overlay = document.getElementById('overlay');
 
+let contactModal = document.getElementById('contact-me-modal');
+let contactMeButton = document.getElementById('contact-me');
+
+contactMeButton.addEventListener('click', openContactInfo)
 modalButton.addEventListener('click', openModal);
 closeButton.addEventListener('click', closeModal);
+
+function openContactInfo() {
+    contactModal.classList.add('active');
+    overlay.classList.add('active');
+    document.body.style.overflow = "hidden";
+}
 
 function openModal() {
     modal.classList.add('active');
@@ -14,6 +24,7 @@ function openModal() {
 
 function closeModal() {
     modal.classList.remove('active');
+    contactModal.classList.remove('active');
     overlay.classList.remove('active');
     document.body.style.overflow = "auto";
 }
