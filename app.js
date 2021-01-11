46f678631ef6e91da8d15c81fd0f2e8397054c0d
jahn18 <john.ahn.es@gmail.com>
2021-01-11 13:40:55 -0800

    Fixed a bit of style and added a new project

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
 let modal = document.getElementById('modal');
+let snowballModal = document.getElementById('snowball-modal');
+
 let modalButton = document.getElementById('motion-design-card');
+let snowballModalButton = document.getElementById('snowball-card');
+
 let closeButton = document.getElementById('closeBtn');
 let overlay = document.getElementById('overlay');
 
-modalButton.addEventListener('click', openModal);
+snowballModalButton.addEventListener('click', openModalSnowball)
+modalButton.addEventListener('click', openModalMotionDesign);
 closeButton.addEventListener('click', closeModal);
 
-function openModal() {
+function openModalSnowball() {
+    snowballModal.classList.add('active');
+    overlay.classList.add('active');
+    document.body.style.overflow = "hidden";
+}
+
+function openModalMotionDesign() {
     modal.classList.add('active');
     overlay.classList.add('active');
     document.body.style.overflow = "hidden";
 }
 
 function closeModal() {
+    snowballModal.classList.remove('active');
     modal.classList.remove('active');
     // contactModal.classList.remove('active');
     overlay.classList.remove('active');
